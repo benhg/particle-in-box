2153b59345a3538aa78eaf618890ec098f3ac47f
Ben Glick <glick@glick.cloud>
2020-02-17 22:45:46 -0800

    add arbitrary number of constants, arbitrary initial shape

--- matlab_translation.py
+++ matlab_translation.py
@@ -1,8 +1,11 @@
 import numpy as np
+from scipy.integrate import quad as integrate
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import quadpy
+from math import sin
 # Define constants and variables
-n = 3;
+n = 7;
 Lx = 1;
 hbar = 1;
 m = 1;
@@ -11,12 +14,12 @@ def k(n):
 	return n*np.pi/Lx;
 
 w = hbar*k(n)**2/2*m;
-A = np.sqrt(2/Lx);
+A_const = np.sqrt(2/Lx);
 t = np.linspace(0,10,50);
 
 # Define the position and time functions
 def psi(x, n):
-	return A*np.sin(k(n)*x)
+	return A_const*np.sin(k(n)*x)
 
 def phi(t):
     return np.exp(-1j*w*t)
@@ -27,6 +30,32 @@ def Psi(x, n, t):
 def Psi_square(x, n, t):
 	psi = Psi(x, n, t)
 	return np.conj(psi) * psi
+
+def A(n, f):
+
+    def integrate_func(x):
+        psi_res = psi(x, n) 
+        fx = f(x)
+        return fx * psi_res
+
+    return integrate(integrate_func, 0, Lx)
+
+def Psi_fourier(f_init, n_a, x, t):
+    A_list = np.zeros(n_a, dtype=np.complex128)
+
+    for i in range(n_a):
+        A_list[i] = A(i, f_init)[0]
+
+    psi_res = 0
+    for i in range(n_a):
+        psi_res += A_list[i]*Psi(x, n, t)
+
+    return psi_res
+
+def Psi_square_fourier(f, n_a, x, t):
+    psi_res = Psi_fourier(f, n_a, x, t)
+    return np.conj(psi_res) * psi_res
+
 
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
@@ -41,8 +70,8 @@ def init():
 
 # animation function.  This is called sequentially
 def animate(i):
-    x = np.linspace(0,Lx,200)
-    y = Psi(x, n, i)
+    x = np.linspace(0, Lx, 200)
+    y = [Psi_fourier(lambda z: -7*np.exp(-1*z), n, x_c, i) for x_c in x] 
     line.set_data(x, y)
     return line,
 
